e9e3bb0e8536186d658af3b9406d2fa1bc9be3a1
Mr-Archi <60386089+Mr-Archi@users.noreply.github.com>
2020-01-28 17:02:02 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    //System.out.println("Hello world!");
-      NotCosCalculator notcoscalculator = new NotCosCalculator ();
-      double result = NotCosCalculator.CalculateWithForLoop(1, 4);
-      System.out.println(result);
- }
-}
