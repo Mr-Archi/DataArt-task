805c21c9bebfe7fa2058c8719f090487fa7528a1
Mr-Archi <60386089+Mr-Archi@users.noreply.github.com>
2020-01-28 13:33:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello world!");
+      NotCosCalculator notcoscalculator = new NotCosCalculator ();
+      double result = NotCosCalculator.CalculateWithForLoop(1, 4);
+      System.out.println(result);
+ }
+}
